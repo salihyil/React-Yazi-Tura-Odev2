7f49134da7aeb7ac3784c4d7e2c123b7b71c9c3d
Salih Yılmaz <salih_yil@live.com>
2021-05-07 18:51:25 +0300

    React-Yazi-Tura-Odev2

--- src/components/CoinFlipper/CoinFlipper.js
+++ src/components/CoinFlipper/CoinFlipper.js
@@ -3,6 +3,7 @@ import Coin from "../Coin/Coin";
 import "./CoinFlipper.css";
 
 class CoinFlipper extends Component {
+
   constructor(props) {
     super(props);
     // State üzerinde paranın başlangıçtaki durumunu veriyoruz, başlangıçta "tura" olsun.
@@ -10,9 +11,28 @@ class CoinFlipper extends Component {
     this.state = {
       side: "tura",
       flipping: false,
+      totalCounter: 0,
+      yaziCounter:0,
+      turaCounter:0
     };
   }
+
   handleClick = () => {
+    
+    let sayi = Math.floor(Math.random() * 2); // 0 ve 1 üretir
+    console.log("sayi: ",sayi);
+    this.setState({totalCounter: this.state.totalCounter+1})
+
+    if(sayi === 1 ){ //sayi 1 gelirse this.state içindeki side'ı yazi olarak güncelle
+      this.setState({side: "yazi"})
+      this.setState({yaziCounter: this.state.yaziCounter+1})
+      
+    }
+    else { //sayi 0 gelirse this.state içindeki side'ı tura olarak güncelle
+      this.setState({side: "tura"})
+      this.setState({turaCounter: this.state.turaCounter+1})
+    }
+
     // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "flipping" durumunu "true" yapıyoruz.
     this.setState({ flipping: true });
     // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "flipping" durmunu tekrar "false" yapıyoruz.
@@ -23,14 +43,17 @@ class CoinFlipper extends Component {
     return (
       <div className="CoinFlipper">
         <h1>Yazı mı Tura mı?</h1>
-        <Coin side={this.state.side} flipping={this.state.flipping} />
+        <Coin 
+          side={this.state.side} 
+          flipping={this.state.flipping} 
+        />
         <button onClick={this.handleClick}>At!</button>
         <p>
           Toplam
-          <strong> 5 </strong>
+          <strong> {this.state.totalCounter}  </strong>
           atıştan
-          <strong> 3 </strong>ü tura
-          <strong> 2 </strong>
+          <strong> {this.state.turaCounter} </strong>ü tura
+          <strong> {this.state.yaziCounter} </strong>
           si yazı geldi.
         </p>
       </div>
